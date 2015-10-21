ef7ab4174de7b9d966cb725ba640679b5650a4f6
boo1ean <boo1ean0807@gmail.com>
2015-10-22 01:57:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+## knex-dal-has-many
+
+Has-many relation mixin for [knex-dal](https://github.com/boo1ean/knex-dal)
+
+## Installation
+
+```
+npm install knex-dal-has-many
+```
+
+## Usage
+
+Given dal configuration
+
+```javascript
+var hasMany = require('knex-dal-has-many');
+var dal = require('knex-dal');
+var itemsDal = require('./items/dal');
+var knex = require('../services/db');
+
+var table = 'grids';
+var createAndUpdateFields = ['title'];
+
+module.exports = dal({
+	table: table,
+	knex: knex,
+	softDeleteColumn: softDeleteColumn,
+	mixins: [
+		hasMany({
+			childDal: itemsDal,
+			childForeignKey: 'grid_id',
+			property: 'items',
+			methods: {
+				createWithItems: 'create',
+				updateWithItems: 'update'
+			}
+		})
+	],
+	defaults: {
+		create: { created_at: 'now', updated_at: 'now' },
+		update: { updated_at: 'now' }
+	},
+	pick: {
+		create: createAndUpdateFields,
+		update: createAndUpdateFields
+	}
+});
+```
+
+Allow to store hierarchical data with one function call
+
+```javascript
+var gridsDal = require('./grids/dal');
+
+gridsDal.createWithItems({
+	title: 'new grid',
+	items: [
+		{ title: 'item 1' },
+		{ title: 'item 2' }
+	]
+});
+```
+Above code will create record in `grids` table and then create two related records in `grid_items` table.
+
+## Available methods
+
+### create
+
+Create record with child records
+
+### update
+
+Update record with child records
---
+++ index.js
@@ -0,0 +1,62 @@
+var _ = require('lodash');
+var assert = require('assert');
+var Promise = require('bluebird');
+
+var methodBuilders = {
+	create: function buildCreateWithChildren (childDal, childrenPropertyName, childForeignKeyColumnName) {
+		return function createWithChildren (params) {
+			return this.create(params)
+				.tap(createOrUpdateItems);
+
+			function createOrUpdateItems (parentId) {
+				return Promise.map(
+					params[childrenPropertyName].map(attachReference),
+					childDal.create
+				);
+
+				function attachReference (item) {
+					item[childForeignKeyColumnName] = parentId;
+					return item;
+				}
+			}
+
+		}
+	},
+
+	update: function buildUpdateWithChildren (childDal, childrenPropertyName, childForeignKeyColumnName) {
+		return function createWithChildren (params) {
+			return this.update(params)
+				.tap(createOrUpdateItems);
+
+			function createOrUpdateItems (parentId) {
+				return Promise.map(
+					params[childrenPropertyName].map(attachReference),
+					childDal.createOrUpdate
+				);
+
+				function attachReference (item) {
+					item[childForeignKeyColumnName] = parentId;
+					return item;
+				}
+			}
+
+		}
+	}
+}
+
+function createMethods (opts) {
+	assert(opts.childDal, 'Child DAL is required for configuring hasMany relation');
+	assert(opts.property, 'property name is required to configure hasMany relation');
+	assert(opts.childForeignKey, 'childForeignKey is required to configure hasMany relation');
+	assert(opts.methods || _.isEmpty(opts.methods), 'Configuring hasMany relation without methods does not make sense');
+
+	var methods = {};
+
+	for (var methodName in opts.methods) {
+		methods[methodName] = methodBuilders[opts.methods[methodName]](opts.childDal, opts.property, opts.childForeignKey);
+	}
+
+	return methods;
+}
+
+module.exports = createMethods;
